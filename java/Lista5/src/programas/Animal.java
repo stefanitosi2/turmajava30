f94087755db07e9e6a3c86faba7db80254809907
Stefani Fernanda Pereira Tosi <stefanitosi2@hotmail.comgit config --global user.namegit config --global user.email stefanitosi2@hotmail.com>
2021-08-09 17:46:38 -0300

    Lista 5 Java

---
+++ java/Lista5/src/programas/Animal.java
@@ -0,0 +1,42 @@
+package programas;
+
+public class Animal {
+
+	
+		private String nome;
+		private int emiteSom;
+		
+		
+		public Animal() {
+			super();
+			this.nome = nome;
+			this.emiteSom = emiteSom;
+		}
+
+
+		public String getNome() {
+			return nome;
+		}
+
+
+		public void setNome(String nome) {
+			this.nome = nome;
+		}
+
+
+		public int getEmiteSom() {
+			return emiteSom;
+		}
+
+
+		public void setEmiteSom(int emiteSom) {
+			this.emiteSom = emiteSom;
+		}
+		
+		
+		
+		
+
+	
+
+}
