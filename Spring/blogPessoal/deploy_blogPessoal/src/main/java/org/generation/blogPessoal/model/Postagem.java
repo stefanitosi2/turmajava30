966a6f69c8d008127767ad3194b1559f7b90918b
stefanitosi2 <stefanitosi2@hotmail.comgit config --global user.namegit config --global user.email stefanitosi2@hotmail.com>
2021-09-14 08:54:53 -0300

    Deploy blog pessoal

---
+++ Spring/blogPessoal/deploy_blogPessoal/src/main/java/org/generation/blogPessoal/model/Postagem.java
@@ -0,0 +1,84 @@
+package org.generation.blogPessoal.model;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+
+
+@Entity
+@Table(name = "postagem")
+public class Postagem {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@NotNull
+	@Size(min = 5, max = 100)
+	private String titulo;
+	
+	@NotNull
+	@Size(min = 10, max = 500)
+	private String texto;
+	
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date date = new java.sql.Date(System.currentTimeMillis());  //para por do tipo java temos que importar do java.util, e o java.sql pega hora, data, minutos, segundos e milessimos
+	
+	@ManyToOne
+	@JsonIgnoreProperties("postagem")
+	private Tema tema;
+	
+	@ManyToOne
+    @JsonIgnoreProperties("postagem")
+    private Usuario usuario;
+	
+	public Tema getTema() {
+		return tema;
+	}
+	public void setTema(Tema tema) {
+		this.tema = tema;
+	}
+	//vamos inserir o GET and Setter, selecionando todos os atributos
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	public String getTexto() {
+		return texto;
+	}
+	public void setTexto(String texto) {
+		this.texto = texto;
+	}
+	public Date getDate() {
+		return date;
+	}
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	
+	
+	
+	
+	
+
+}
